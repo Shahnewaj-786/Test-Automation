8a9cac2d062d448b8986f4fae97e4f7b1c581286
Shahnewaj-786 <shahanewajshaikh786@gmail.com>
2024-05-13 00:04:24 +0600

    variables

--- Road to SDET (Advance Automation)/Node  JS/index.js
+++ Road to SDET (Advance Automation)/Node  JS/index.js
@@ -1,1 +1,38 @@
 console.log  ("Start Basic Node js");
+
+
+var  x = 25;
+var x = 30;
+
+
+let y = "Variable";
+
+let z = 90; 
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+
+console.log("Variable type is: " + typeof(y));
+
+
+
+let a = 50;
+let b = 90;
+a =  undefined;
+b = null;
+console.log(a);
+console.log(b);
+
+
+let obj = {
+    name : "Boba",
+    age : 30
+};
+
+console.log("This is an object: ");
+console.log(obj);
+
+
