20ca709dc22de8085d2cb3ec88b3d53aba09f886
Shahnewaj-786 <shahanewajshaikh786@gmail.com>
2024-05-14 00:34:41 +0600

    Object

--- Road to SDET (Advance Automation)/Node  JS/index.js
+++ Road to SDET (Advance Automation)/Node  JS/index.js
@@ -1,22 +1,15 @@
 console.log  ("Start Basic Node js");
 
-
 var  x = 25;
 var x = 30;
 
-
 let y = "Variable";
-
 let z = 90; 
 
 console.log(x);
 console.log(y);
 console.log(z);
-
-
-
 console.log("Variable type is: " + typeof(y));
-
 
 
 let a = 50;
@@ -36,3 +29,19 @@ console.log("This is an object: ");
 console.log(obj);
 
 
+const people = {
+    name : "Tuni",
+    age : 2,
+    salary : "100 TK",
+    day : 5,
+    address : {
+        home : "Dhaka",
+        office : "Mirpur"
+    }
+};
+
+people.name = "Bappi";
+people.address.home =  "Gopalganj";
+people.salaryAccount = "Brac Bank";
+
+console.log(people);
